a7bf27427c954487ab2d5a457eb99568c6186603
HenriqueSSan <hsstudentwork@gmail.com>
2026-01-15 15:31:16 -0300

    feat: added persist task list

--- src/module_core/module_todo/context/todo.context.ts
+++ src/module_core/module_todo/context/todo.context.ts
@@ -11,18 +11,18 @@ export const TodoContext = createContext(
   {} as {
     todos: Task[];
     editId: number | null;
-    showEditDialog: boolean;
     delTodo: (currentTodo: { id: number }) => void;
+    editTodo: (currentTodo: Pick<Task, 'id' | 'title' | 'description'>) => void;
     setTodos: React.Dispatch<SetStateAction<Task[]>>;
+    setEditId: React.Dispatch<SetStateAction<number | null>>;
     addedNewTodo: (params: { title: string }) => void;
     editFormData: { title: string; description?: string };
-    setEditId: React.Dispatch<SetStateAction<number | null>>;
-    toggleCompletedTodo: (currentTodo: { id: number }) => void;
-    setShowEditDialog: React.Dispatch<SetStateAction<boolean>>;
+    showEditDialog: boolean;
+    setEditFormData: React.Dispatch<SetStateAction<{ title: string; description?: string }>>;
     delByDataTodoId: (e: MouseEvent<HTMLButtonElement>) => void;
     editByDataTodoId: (e: MouseEvent<HTMLButtonElement>) => void;
+    setShowEditDialog: React.Dispatch<SetStateAction<boolean>>;
+    toggleCompletedTodo: (currentTodo: { id: number }) => void;
     toggleByDataCompletedTodoId: (e: ChangeEvent<HTMLInputElement>) => void;
-    editTodo: (currentTodo: Pick<Task, 'id' | 'title' | 'description'>) => void;
-    setEditFormData: React.Dispatch<SetStateAction<{ title: string; description?: string }>>;
   },
 );
